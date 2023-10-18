7452101c161bd3cc5029ba24ff6fc56dbbf49551
Viktor Gonchar <vita-sc2@mail.ru>
2023-10-19 02:34:24 +0300

    add hw20 19.10.23

--- base.py
+++ base.py
@@ -1,32 +1,29 @@
-# in less
-
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 
 
 def wait_for_clicable_css(driver, locator):
     """
     f
-    :param driver:
-    :param locator:
-    :return:
     """
-    return WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, locator)),
-        "Element not clicable")
+    try:
+        return WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, locator)))
+    except WebDriverException:
+        assert False, f"Element {locator} not clicable"
 
 
 def wait_for_clicable_xpath(driver, locator):
     """
     f
-    :param driver:
-    :param locator:
-    :return:
     """
-    return WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, locator)),
-        "Element not clicable")
+    try:
+        return WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, locator)))
+    except WebDriverException:
+        assert False, f"Element {locator} not clicable"
 
 
 def add_cookie(driver, name, value):
@@ -39,8 +36,13 @@ def delete_cookies(driver):
     driver.refresh()
 
 
+def find_element_func(driver, locator):
+    element = driver.find_element(By.CSS_SELECTOR, locator)
+    return element
+
+
 def assert_text_in_element(driver, locator, expected_result):
-    element = driver.find_element(By.XPATH, locator)
+    element = driver.find_element(By.CSS_SELECTOR, locator)
     assert element.text == expected_result, "Text not the same"
 
 
@@ -57,7 +59,9 @@ def hard_click(driver, locator):
 
 
 def fill(driver, locator, text):
-    element = driver.find_element(By.XPATH, locator)
+    element = driver.find_element(By.CSS_SELECTOR, locator)
     element.clear()
     element.sendKeys(text)
-    element.sendKeys('')
+    # element.sendKeys('')
+
+
--- tests/test_hw20_task1.py
+++ tests/test_hw20_task1.py
@@ -5,35 +5,6 @@ from base import wait_for_clicable_css, wait_for_clicable_xpath
 from conftest import driver_chrome
 
 
-# создать минимум пять тестов для выбранного вами сайта
-# добавить документацию к тестам, чтобы можно было понять о чем тест
-
-
-# Задание 1:
-#
-# 1. Открыть сайт http://thedemosite.co.uk/login.php
-#
-# 2. Ввести имя в поле username
-#
-# 3. Ввести пароль в поле password
-#
-# 4. Нажать на кнопку Test Login
-#
-# 5. Проверить, что Successful Login отображаются
-#
-# Задание 2
-#
-# 1. Открыть сайт http://demo.guru99.com/test/newtours/register.php
-#
-# 2. Заполнить все поля
-#
-# 3. Нажать кнопку Submit
-#
-# 4. Проверить, что отображается правильно имя и фамилия
-#
-# 5. Проверить, что отображается правильно username
-
-
 locator_the_cross = 'main>div>div>div>[aria-label="Close"]'
 locator_profile = 'nav>div>ul>li>[href="#loginModal"]'
 locator_profile_authorized_user = '[class="nav-link d-flex align-items' \
@@ -51,12 +22,20 @@ locator_vk_icon = '[class="social-link"]>[src="https://www.kfc.by/admin/' \
                   'files/4475.svg"]'
 locator_reject_cookies = '[class="tap decline js-close"]'
 locator_orders = '[class="nav-item d-none d-xl-block"]>[href="/tracker"]'
-locator_add_to_basket_button = ''
-locator = '//*[@id="categories-panels"]/div[1]/div[2]/div[2]/div/div[6]/div' \
-          '/div/div/div[3]/div/div[3]'
-#############################
-text_login = 'vita-sc2@mail.ru'  # test@mail.ru  введите логин
-text_password = 'Zpo230742Er'  # 12345678        введите пароль
+locator_add_to_basket_button = '//*[@id="categories-panels"]/div[1]/div[2]/d' \
+                               'iv[2]/div/div[6]/div/div/div/div[3]/div/div[3]'
+locator_choosing_a_restaurant = '[data-content="Выберите ресторан"]'
+locator_restaurant_address = '[data-store-id="94"]'
+locator_of_the_start_your_order_button = '[class="startorder-btn mainBgColor' \
+                                         ' mainBorderColor takeAwayModalBtn"]'
+locator_checkout_button = 'div>div>div>[class="checkoutBtn mainBgColor"]'
+locator_checking_the_order_confirmation = 'div>div>div>[class="checkoutBtn' \
+                                          ' mainBgColor"]'
+locator_address_display = '[id="newAddress_address"]'
+locator_add_address_button = '[class="addAddressBtn mainBgColor newAddAddre' \
+                             'ss"]'
+text_login = 'введите свой логин'
+text_password = 'введите свой пароль'
 
 
 @pytest.fixture(scope='function')
@@ -71,7 +50,7 @@ def authorization_kfc(driver_chrome):
         6. Close the pop-up banner again
         7. Wait for an item available to an authorized user to load
     """
-    a = driver_chrome.get('https://www.kfc.by')
+    driver_chrome.get('https://www.kfc.by')
     the_cross = wait_for_clicable_css(driver_chrome, locator_the_cross)
     the_cross.click()
     profile = wait_for_clicable_css(driver_chrome, locator_profile)
@@ -86,6 +65,7 @@ def authorization_kfc(driver_chrome):
     the_cross.click()
     profile = wait_for_clicable_css(
         driver_chrome, locator_profile_authorized_user)
+    assert EC.element_to_be_clickable(profile)
 
 
 class TestKFC:
@@ -140,13 +120,32 @@ class TestKFC:
 
         assert link_to_click_vk_icon == link_to_vk_expected
 
-
-
-    def test_a(self, driver_chrome, authorization_kfc):
+    def test_address_display(self, driver_chrome, authorization_kfc):
         """
-        Сделать заказ
+        Precondition: the user must be logged in to the site
+        'https://www.kfc.by' !
+        Checking that the entered text corresponds to the expected result
         """
-        driver_chrome.get('https://www.kfc.by/menu')
-        add_to_basket_button = wait_for_clicable_xpath(driver_chrome,
-                                                 locator_add_to_basket_button)
+        driver_chrome.implicitly_wait(10)
+        driver_chrome.get('https://www.kfc.by/account#addresses')
+        add_address_button = wait_for_clicable_css(
+            driver_chrome, locator_add_address_button)
+        add_address_button.click()
+        address_display = wait_for_clicable_css(
+            driver_chrome, locator_address_display)
+        address_display.send_keys('Минск, Беларусь')
+        text = address_display.get_attribute('value')
 
+        assert text == 'Минск, Беларусь'
+
+    def test_profile_icon_selected(self, driver_chrome, authorization_kfc):
+        """
+        Precondition: the user must be logged in to the site
+        'https://www.kfc.by' !
+        Checking that when you click on the profile icon, it is selected
+        """
+        profile = wait_for_clicable_css(
+            driver_chrome, locator_profile_authorized_user)
+        profile.click()
+
+        assert EC.element_to_be_selected(profile)
